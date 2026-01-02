3c408b7dd8acbf0a01bfb8102cb1f1b01e845f84
sseung0510 <choiseunghi0510@gmail.com>
2026-01-02 12:15:04 +0900

    ♻ refactor: 사용하지 않는 import문 삭제
    - CommentRepository에서 사용하지 않는 import문 삭제

--- src/main/java/com/example/sixpark/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/sixpark/domain/comment/repository/CommentRepository.java
@@ -2,9 +2,6 @@ package com.example.sixpark.domain.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.sixpark.domain.comment.entity.Comment;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
