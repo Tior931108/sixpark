9fcd7036e02ac9186e66ccd117f3a3a8c6134a38
Tior931108 <jyj8469@gmail.com>
2026-01-09 01:01:08 +0900

    🐛 fix: redis > DB 스케줄 분산 락 원래 로직 코드로 수정
    - 예외 처리가 발생해도 분산락은 해제가 되어야함.
    - 추가로 redis 스케줄러 적용한 메소드 어노테이션 오타 수정

--- src/main/java/com/example/sixpark/common/lock/RedisScheduleLockAspect.java
+++ src/main/java/com/example/sixpark/common/lock/RedisScheduleLockAspect.java
@@ -35,12 +35,9 @@ public class RedisScheduleLockAspect {
         // 비즈니스 로직에서 예외가 발생해도 락은 반드시 해제되어야 하기때문에 TTL이 있어도 바로 해제
         try {
             return joinPoint.proceed();  // 메서드 발생
-        } catch (RuntimeException e) {
-            throw e; // CustomException 포함
-        } catch (Throwable t) {
-            throw new RuntimeException(t); // 정말 예측 불가한 예외만
+        } finally {
+            redisTemplate.delete(scheduleLock.key());
         }
-
     }
 
 }
